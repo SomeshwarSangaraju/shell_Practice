238200281df2c727a3e593b58ec1dfddec3b5104
SSR <someshwar.sangaraju19@gmail.com>
2026-02-11 19:59:58 +0530

    shell scripting

---
+++ 10-for-loop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]; then
+    echo "Error:: please run this with root access"
+    exit 1
+fi
+
+VALIDATES(){
+    if [ $? -ne 0 ]; then
+        echo "$1 installation is failed"
+        exit 1
+    else
+        echo "$1 installation is success"
+    fi
+}
+
+for pkg in "$@"
+do 
+    dnf list installed $pkg 
+    if [ $? -ne 0 ]; then
+        dnf install $pkg -y
+        VALIDATES "$pkg"
+    else
+        echo "$pkg already exist"
+done
