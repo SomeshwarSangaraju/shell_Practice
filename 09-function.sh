4c02024cb5ba9f5ce4beda1bf1dfac71d4f31fdd
SSR <someshwar.sangaraju19@gmail.com>
2026-02-11 19:30:44 +0530

    shell scripting

---
+++ 09-function.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [$USER]; then
+    echo "ERROR:: Please run this with root access"
+    exit 1
+fi
+
+VALIDATES(){
+    if [ $? -ne 0]; then
+        echo "$1 installation is success"
+    else
+        echo "$1 installation is failed"
+    fi
+}
+dnf install mysql -y
+VALIDATES mysql
+
+
+dnf install nginx -y
+VALIDATES nginx
+
+dnf install mongodb -y
+VALIDATES mongodb
