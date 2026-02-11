fc83e53a2b1fa523d36374adb2f71eb886b024ac
SSR <someshwar.sangaraju19@gmail.com>
2026-02-11 19:39:05 +0530

    shell scripting

--- 09-function.sh
+++ 09-function.sh
@@ -8,18 +8,18 @@ if [ $USER -ne 0 ]; then
 fi
 
 VALIDATES(){
-    if [ $? -ne 0 ]; then
-        echo "$1 installation is success"
+    if [ $1 -ne 0 ]; then
+        echo "$2 installation is success"
     else
-        echo "$1 installation is failed"
+        echo "$2 installation is failed"
     fi
 }
-dnf install mysql -y
-VALIDATES mysql
 
+dnf install mysql -y
+VALIDATES $? mysql
 
 dnf install nginx -y
-VALIDATES nginx
+VALIDATES $? nginx
 
 dnf install mongodb -y
-VALIDATES mongodb
+VALIDATES $? mongodb
