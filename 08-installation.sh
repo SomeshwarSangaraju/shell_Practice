c3b7305074e1df44fe3497913df37f0094e38dfb
SSR <someshwar.sangaraju19@gmail.com>
2026-02-11 19:12:01 +0530

    shell scripting

--- 08-installation.sh
+++ 08-installation.sh
@@ -4,4 +4,13 @@ USER=$(id -u)
 
 if [$USER -ne 0]; then
     echo "Install with root access"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0]; then
+    echo "mysql installation failed"
+else
+    echo "mysql installation success"
 fi
