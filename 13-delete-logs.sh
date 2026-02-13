b08e96c090ff5501ff4b90ce7f4b1edac4e6e3c4
SSR <someshwar.sangaraju19@gmail.com>
2026-02-13 15:00:45 +0530

    shell scripting

--- 13-delete-logs.sh
+++ 13-delete-logs.sh
@@ -6,9 +6,22 @@ LOGS_FILE=$LOGS_FOLDER/$LOG_FILE.log
 
 mkdir -p $LOG_FILE
 
-if [ ! -d $LOG_FOLDER -ne 0 ]; then
+SOURCE_DIR=/home/ec-user/app-logs
+
+if [ ! -d $SOURCE_DIR -ne 0 ]; then
     echo "ERROR:: $1 file does not exists"
     exit 1
 fi
 
-find . -name $SCRIPT_NAME -type f -mtime +14
+# FILE_TO_DELETE=$(find . -name -type f -mtime +14)
+
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+
+while IFS= read -r filepath
+do
+    echo "Deleting the file: $filepath"
+    rm -rf $filepath
+    echo "Deleted the file: $filepath"
+done <<< $FILES_TO_DELETE
+
