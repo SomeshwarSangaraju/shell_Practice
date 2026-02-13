5853172ac492ed24cdf563fe3d11c66ce5bca0d0
SSR <someshwar.sangaraju19@gmail.com>
2026-02-13 15:11:13 +0530

    shell scripting

--- 06-executionTime.sh
+++ 06-executionTime.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Start_Time=$(Date-%s)
+Start_Time=$(Date +%s)
 Sleep 10 
-End_Time=$(date-%s)
+End_Time=$(date +%s)
 Total_Time=$(($End_Time-$Start_Time))
 
 echo "Total Execution Time: $Total_Time"
