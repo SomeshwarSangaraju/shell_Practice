02e1ac5b236bf94ad19f9c2843306b772605867a
SSR <someshwar.sangaraju19@gmail.com>
2026-02-13 15:09:02 +0530

    shell scripting

--- 13-delete-logs.sh
+++ 13-delete-logs.sh
@@ -5,7 +5,8 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+START_TIME=$(date)
+echo "Script started executed at: $START_TIME" | tee -a $LOG_FILE
 
 SOURCE_DIR=/home/ec2-user/app-logs
 
@@ -25,3 +26,9 @@ do
     rm -rf $filepath
     echo "Deleted the file: $filepath"
 done <<< $FILES_TO_DELETE
+
+END_TIME=$(date)
+echo "Script started executed at: $END_TIME" | tee -a $LOG_FILE
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Script end executed at: $TOTAL_TIME" | tee -a $LOG_FILE
