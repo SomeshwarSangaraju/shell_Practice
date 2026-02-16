e7ff619de886aa63bad655467ca0a13f9ceba7a7
SSR <someshwar.sangaraju19@gmail.com>
2026-02-16 19:27:46 +0530

    shell scripting

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -3,7 +3,10 @@
 Disk_Usage=$(df -hT | grep -v Filesystem ) 
 Disk_Threshold=75
 
-while IFS=read -r file
+while IFS= read -r file
 do
     echo "file : $file"
+    usage=$(awk '{print $6}' | cut -d "%" -f1 )
+    Partition=$(awk '{print $7}')
+
 done <<< $Disk_Usage
