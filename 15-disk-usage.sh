1bfc4d5a894f4899914b6c2856f08a10548d6a61
SSR <someshwar.sangaraju19@gmail.com>
2026-02-16 19:34:22 +0530

    shell scripting

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -9,7 +9,7 @@ do
     Usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1 )
     Partition=$(echo $line | awk '{print $7}')
     if [ $Usage -gt $DISK_THRESHOLD ]; then
-       message+=echo " $PARTITION : $Usage" 
+       message+=echo " $Partition : $Usage" 
     fi
 
 done <<< $Disk_Usage
