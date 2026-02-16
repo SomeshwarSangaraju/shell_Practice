48c399747e9d4d4f5bf15c1a3497cd612c1afe18
SSR <someshwar.sangaraju19@gmail.com>
2026-02-16 19:31:11 +0530

    shell scripting

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -5,11 +5,10 @@ Disk_Threshold=75
 
 while IFS= read -r file
 do
-    # echo "file : $file"
     Usage=$(awk '{print $6}' | cut -d "%" -f1 )
     Partition=$(awk '{print $7}')
-    # if [ $Usage -gt $DISK_THRESHOLD ]; then
-    #     echo "High disk usage"
-    # fi
+    if [ $Usage -gt $DISK_THRESHOLD ]; then
+        echo "High disk usage"
+    fi
 
 done <<< $Disk_Usage
