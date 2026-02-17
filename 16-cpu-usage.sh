6ccf604f6723ef2d0ce0e50719d7dc63cea1752f
SSR <someshwar.sangaraju19@gmail.com>
2026-02-17 20:33:22 +0530

    shell scripting

--- 16-cpu-usage.sh
+++ 16-cpu-usage.sh
@@ -9,6 +9,8 @@ do
     USAGE=$(echo $file | awk '{print $6}' | cut -d "%" -f1 )
     PARTITION=$(echo $file | awk '{print $7}' )
     if [ $USAGE -gt $DISK_THRESHOLD ]; then
-        MESSAGE+=" High disk usage on $PARTITION : $USAGE <br>"
+        MESSAGE+="High disk usage on $PARTITION : $USAGE <br>"
     fi
 done <<< $DISK_USAGE
+
+echo "Message : $MESSAGE"
