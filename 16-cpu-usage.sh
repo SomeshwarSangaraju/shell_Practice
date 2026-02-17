e3cfcd0c239ec6f08f52ba8102ea01bd9e6e4b75
SSR <someshwar.sangaraju19@gmail.com>
2026-02-17 20:31:20 +0530

    shell scripting

--- 16-cpu-usage.sh
+++ 16-cpu-usage.sh
@@ -2,9 +2,13 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=75
+MESSAGE=""
 
 while IFS= read -r file
 do 
-    echo "$file"
-    
+    USAGE=$(echo $file | awk '{print $6}' | cut -d "%" -f)
+    PARTITION=$(echo $file | awk '{print $7}')
+    if [ $USAGE -gt $DISK_THRESHOLD ]; then
+        MESSAGE+=" High disk $PARTITION : $USAGE"
+    fi
 done <<< $DISK_USAGE
